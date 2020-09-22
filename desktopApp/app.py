f579336ee3945b14759704b005243f0cae05a945
jaimecuellar14 <jamescuellar14@gmail.com>
2020-09-22 18:09:01 +0200

    Finalizacion proyecto (ambos modelos)

--- desktopApp/app.py
+++ desktopApp/app.py
@@ -22,19 +22,30 @@ from mrcnn.visualize import display_images
 import mrcnn.model as modellib
 from mrcnn.model import log
 
-from samples.balloon import balloon
+from samples.balloon import estadios
+from samples.africa import africa
 from mrcnn import config
 
 MODEL_DIR = "./Modelo/tfg_notebook/main_1/Mask_RCNN-master/logs"
+MODEL_AFRICA_DIR = "./Modelo/tfg_notebook/main/Mask_RCNN-master/logs"
 DEVICE = "cpu/:0"
 
-config = balloon.BalloonConfig()
+config = estadios.BalloonConfig()
+config_africa = africa.BalloonConfig()
 class InferenceConfig(config.__class__):
 			GPU_COUNT=1
 			IMAGES_PER_GPU=1
 
 config = InferenceConfig()
 config.display()
+
+class InferenceConfigAfrica(config_africa.__class__):
+			GPU_COUNT=1
+			IMAGES_PER_GPU=1
+		
+config_africa = InferenceConfigAfrica()
+config_africa.display()
+
 class MyApp(wx.App):
 	def __init__(self):
 		super().__init__(clearSigInt=True)
@@ -59,41 +70,95 @@ class Panel(wx.Panel):
 
 		welcomeText = wx.StaticText(self, id=wx.ID_ANY,label="Modelo de reconocimiento de imagenes aereass", pos=(20,20))
 
-		predictButton = wx.Button(parent=self, label = "Realizar prediccion", pos=(20,80))
+		predictButton = wx.Button(parent=self, label = "Realizar prediccion (hospitales,estadios)", pos=(20,80))
 		predictButton.Bind(event=wx.EVT_BUTTON, handler=self.predecir)
+
+		predictButtonAfrica = wx.Button(parent=self, label="Prediccion zonas África",pos=(20,120))
+		predictButtonAfrica.Bind(event=wx.EVT_BUTTON,handler=self.predecirAfrica)
+
+	def predecirAfrica(self,event):
+		print("predecir Africa")
+		config_africa = africa.BalloonConfig()
+		config_africa = InferenceConfigAfrica()
+		config_africa.display()
+		DATA_DIR = "./Modelo/tfg_notebook/main/dataset"
+		BALLOON_DIR = DATA_DIR
+		dataset_africa = africa.BalloonDataset()
+
+		with wx.FileDialog(self, "Selecciona una imagen para predecir su clase", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST ) as fileDialog:
+			if fileDialog.ShowModal() == wx.ID_CANCEL:
+				return
+			pathname = fileDialog.GetPath()
+			try:
+				with open(pathname,'r') as file:
+					print(pathname)
+					dataset_africa.load_balloon(BALLOON_DIR,"val")
+					
+					dataset_africa.prepare()
+					print("Images: {}\nClasses: {}".format(len(dataset_africa.image_ids), dataset_africa.class_names))
+					config_africa = InferenceConfigAfrica()
+					
+					with tf.device(DEVICE):
+						model = modellib.MaskRCNN(mode="inference", model_dir = MODEL_AFRICA_DIR, config=config_africa)
+
+					weights_path = model.find_last()
+					
+					model.load_weights(weights_path, by_name=True)
+					image = skimage.io.imread(pathname)
+					plt.figure(figsize=(12,10))
+					skimage.io.imshow(image)
+					plt.show()
+
+					result = model.detect([image],verbose=1)
+					r = result[0]
+					visualize.display_instances(image, r['rois'],r['masks'],r['class_ids'],dataset_africa.class_names, r['scores'])
+					
+			except IOError:
+				wx.LogError("ERROR")
 
 	def predecir(self, event):
 		print("Funciono boton")
-		#config = balloon.BalloonConfig()
-		#config = InferenceConfig()
-		#config.display()
+		config = estadios.BalloonConfig()
+		config = InferenceConfig()
+		config.display()
 		DATA_DIR = "./Modelo/tfg_notebook/main_1/dataset"
 		BALLOON_DIR = DATA_DIR
-		dataset = balloon.BalloonDataset()
-		dataset.load_balloon(BALLOON_DIR,"val")
-		dataset.prepare()
-		print("Images: {}\nClasses: {}".format(len(dataset.image_ids), dataset.class_names))
-		config = InferenceConfig()
+		dataset = estadios.BalloonDataset()
+		with wx.FileDialog(self,"Selecciona una imagen para predecir su clase" , style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+			if fileDialog.ShowModal() == wx.ID_CANCEL:
+				return
+			pathname = fileDialog.GetPath()
+			try:
+				with open(pathname, 'r') as file:
+					print(pathname)
+					dataset.load_balloon(BALLOON_DIR,"val")
+					dataset.prepare()
+					print("Images: {}\nClasses: {}".format(len(dataset.image_ids), dataset.class_names))
+					config = InferenceConfig()
+					
+					with tf.device(DEVICE):
+						model = modellib.MaskRCNN(mode="inference", model_dir = MODEL_DIR, config=config)
+					
+
+					weights_path = model.find_last()
+					print("Loading weights ", weights_path)
+					model.load_weights(weights_path, by_name=True)
+
+					#image = skimage.io.imread("./Modelo/tfg_notebook/main_1/dataset/val/estadio-25.JPG")
+
+					image = skimage.io.imread(pathname)
+					plt.figure(figsize=(12,10))
+					skimage.io.imshow(image)
+					plt.show()
+
+					result = model.detect([image],verbose=1)
+					r = result[0]
+					visualize.display_instances(image, r['rois'],r['masks'],r['class_ids'],dataset.class_names, r['scores'])
+			except IOError:
+				wx.LogError("No se pudo abrir el archivo")
 		
-		with tf.device(DEVICE):
-			model = modellib.MaskRCNN(mode="inference", model_dir = MODEL_DIR, config=config)
+			
 		
-
-		weights_path = model.find_last()
-		print("Loading weights ", weights_path)
-		model.load_weights(weights_path, by_name=True)
-
-		#image = skimage.io.imread("./Modelo/tfg_notebook/main_1/dataset/val/estadio-25.JPG")
-
-		image = skimage.io.imread("./images/estadio-24.JPG")
-		plt.figure(figsize=(12,10))
-		skimage.io.imshow(image)
-		plt.show()
-
-		result = model.detect([image],verbose=1)
-		r = result[0]
-		visualize.display_instances(image, r['rois'],r['masks'],r['class_ids'],dataset.class_names, r['scores'])
-
 if __name__ == "__main__":
 	app = MyApp()
 	app.MainLoop()
